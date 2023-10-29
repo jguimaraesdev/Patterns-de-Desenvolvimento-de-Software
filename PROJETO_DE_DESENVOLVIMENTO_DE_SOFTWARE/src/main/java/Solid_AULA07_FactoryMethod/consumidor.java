c2e6c62612b0f8b77707335aa9cce116871e361a
Jhonny <111026602+jguimaraesdev@users.noreply.github.com>
2023-10-28 22:49:02 -0300

    OK

--- PROJETO_DE_DESENVOLVIMENTO_DE_SOFTWARE/src/main/java/Solid_AULA07_FactoryMethod/consumidor.java
+++ PROJETO_DE_DESENVOLVIMENTO_DE_SOFTWARE/src/main/java/Solid_AULA07_FactoryMethod/consumidor.java
@@ -1,12 +1,12 @@
 package Solid_AULA07_FactoryMethod;
 
 public class consumidor {
-	private iFabrica fabrica;
+	private iFabrica Fabrica;
 	public iProduto produtoA;
 	public iProduto produtoB;
 	
 	consumidor(iFabrica fabrica){
-		this.fabrica = fabrica;
+		this.Fabrica = fabrica;
 		this.produtoA = fabrica.fabricarProdutoA();
 		this.produtoB = fabrica.fabricarProdutoB();
 	}
